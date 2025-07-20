494e277a1f929bb147f385b8cb6bb14849a0cf54
Hest <hestdatabase@gmail.com>
2025-07-20 14:58:25 -0300

    Pequenas mudanças

--- src/repositories/ITablesRepository.ts
+++ src/repositories/ITablesRepository.ts
@@ -3,4 +3,5 @@ import { Table } from "@/entities/Table";
 export interface ITablesRepository {
   save(table: Table): Promise<void>;
   list(): Promise<Table[]>;
+  update(id: string, changes: Partial<Table>): Promise<void>;
 }
--- src/repositories/in-memory/InMemoryTablesRepository.ts
+++ src/repositories/in-memory/InMemoryTablesRepository.ts
@@ -11,4 +11,12 @@ export class InMemoryTablesRepository implements ITablesRepository {
   async list(): Promise<Table[]> {
     return this.tables;
   }
+  
+  async update(id: string, changes: Partial<Table>) {
+    this.tables.forEach(table => {
+      if (table.id === id) {
+        Object.assign(table, changes);
+      }
+    });
+  }
 }
